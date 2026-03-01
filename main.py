57af7093c9859284e16bd7ebdcdc0d283d5ae9a3
sandeepbudhathoki50-j <sandeepbudhathoki50@gmail.com>
2026-03-01 20:51:55 +0545

    Add read_log_file function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import datetime
 
-# Known safe IP addresses
 KNOWN_IPS = ["192.168.1.10", "10.0.0.5"]
 FAILED_LOGIN_THRESHOLD = 3
 ABNORMAL_START_HOUR = 0
 ABNORMAL_END_HOUR = 5
+
+def read_log_file(filename):
+    with open(filename, "r") as file:
+        logs = file.readlines()
+    return logs
